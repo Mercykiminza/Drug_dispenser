ec88004143c53cd522413a78ec429ca70d2424c4
Mercykiminza <kiminzamercy95@gmail.com>
2023-06-21 13:53:00 +0300

    Created Utilities and Associated Files

---
+++ application/configuration.php
@@ -0,0 +1,8 @@
+<?php
+$config = [
+	'db_host' => 'localhost',
+	'db_username' => 'root',
+	'db_password' => 'MySQLXXX-123a8910',
+	'db_name' => 'mercy'
+];
+?>
---
+++ application/registeredPatients.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Registered Patients</title>
+    <link rel="stylesheet" type="text/css" href="styles.css">
+    <script src="pagination.js"></script>
+</head>
+<body>
+    <div class="container">
+        <h2>Registered Patients</h2>
+        <table>
+            <thead>
+                <tr>
+                    <th>First Name</th>
+                    <th>Last Name</th>
+                    <th>Email Address</th>
+                    <th>Phone Number</th>
+                    <th>Location</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                require_once '../database.php';
+
+                // Create a new instance of the Database class
+                $db = new Database();
+
+                // Query the patient table to retrieve all patient records
+                $query = "SELECT * FROM patient";
+                $patients = $db->queryData($query, []);
+
+                // Iterate over the patient records and populate the table rows dynamically
+                foreach ($patients as $patient) {
+                    $patientId = $patient['patientId'];
+                    $firstName = $patient['firstName'];
+                    $lastName = $patient['lastName'];
+                    $emailAddress = $patient['emailAddress'];
+                    $phoneNumber = $patient['phoneNumber'];
+                    $location = $patient['location'];
+                ?>
+                <tr>
+                    <td><a href="patientProfile.php?patientId=<?php echo $patientId; ?>"><?php echo $firstName; ?></a></td>
+                    <td><?php echo $lastName; ?></td>
+                    <td><a href="mailto:<?php echo $emailAddress; ?>"><?php echo $emailAddress; ?></a></td>
+                    <td><a href="tel:<?php echo $phoneNumber; ?>"><?php echo $phoneNumber; ?></a></td>
+                    <td><a href="https://www.google.com/maps/search/?api=1&query=<?php echo urlencode($location); ?>" target="_blank"><?php echo $location; ?></a></td>
+                </tr>
+                <?php
+                }
+
+                // Disconnect from the database
+                $db->disconnect();
+                ?>
+            </tbody>
+        </table>
+    </div>
+</body>
+</html>
